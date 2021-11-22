d65513ff19f597b137d64cef8c90c0805f83ce46
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-11-22 15:53:03 +0100

    Up to step 6 (including)

--- api/src/main/java/com/switchfully/maven/exchange/api/StockExchangeController.java
+++ api/src/main/java/com/switchfully/maven/exchange/api/StockExchangeController.java
@@ -2,7 +2,14 @@ package com.switchfully.maven.exchange.api;
 
 import com.switchfully.maven.exchange.domain.Stock;
 import com.switchfully.maven.exchange.service.StockService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@ResponseBody
+@RequestMapping("/stocks") // Search for myself what they do and mean :eyes:
 public class StockExchangeController {
 
     private final StockService stockService;
@@ -18,7 +25,8 @@ public class StockExchangeController {
      * This method gets called from outside this application
      * (not really, but just imagine it will)
      */
-    public StockDto getStock(String stockId) {
+    @GetMapping("/{stockId}") // Added this because I was told to
+    public StockDto getStock(@PathVariable String stockId) {
         Stock foundStock = stockService.getStock(stockId);
         return stockMapper.mapToDto(foundStock);
     }
